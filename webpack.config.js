2cf895dd6a08a16315ca4fab654ea8b454d9ff85
Aboy <e.aboy@sinteac.es>
2020-06-11 20:46:21 +0200

    Add webpack and initila webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'cleanConsole',
+    libraryTarget: 'var'
+  },
+};
